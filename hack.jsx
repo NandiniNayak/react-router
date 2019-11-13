e4a8a45475e2e46ad52370331b84f5dfaf0fa81b
Arthur Truong <37134133+ArthurTruong5@users.noreply.github.com>
2019-11-13 16:46:14 +1100

    Create hack.jsx

---
+++ hack.jsx
@@ -0,0 +1,26 @@
+import react, {useState} from "React";
+
+const nandIsCool = 
+[
+{
+  arthur: "is a legend, its",
+  boolean: true
+},
+{
+  nands: "is also a legend, its sooo",
+  boolean: true
+}
+]
+
+const ChristmasParty = () => {
+
+const [isCool, setIsCool] = useState("")
+
+return(
+<>
+  <p>I'm doing nothing at work :(</p>
+</>
+)
+}
+
+export defaults ChristmasParty;
